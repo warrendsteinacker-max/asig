055af970389fe6b3d37cc6eb956d782cb9805356
Warren <warrendsteinacker@gmail.com>
2026-02-14 09:18:28 -0500

    nnnn

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <--- THIS LINE IS MISSING
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(), // Now this will work!
   ],
-  base: '/asig/'
+  base: '/asig/' 
 })
